b71fd5808a2c678355488737fa9bcf391d4e6216
Dmitriy Sloboda <dmitriy.sloboda@gmail.com>
2020-09-24 20:24:29 +0300

    3-module is done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,24 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(',');
+  let strWithSpace = arr.join(' ');
+  let arrWithSpace = strWithSpace.split(' ');
+  let arrNum = [];
+
+  for (const item of arrWithSpace) {
+
+    if (!isNaN(+item) && +item !== " ") {
+      arrNum.push(item);
+    }
+  }
+
+  arrNum.sort((a, b) => a - b);
+
+  let result = {
+    min: +arrNum[0],
+    max: +arrNum[arrNum.length - 1]
+  }
+
+  return result;
 }
