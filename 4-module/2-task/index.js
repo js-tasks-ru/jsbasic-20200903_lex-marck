a2782a67fe61c6164093f71afa366bcbb09f4eee
Dmitriy Sloboda <dmitriy.sloboda@gmail.com>
2020-09-27 14:00:17 +0300

    4 module 1 and 2 tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,22 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  let count = 0;
+  let row = document.querySelectorAll('tr');
+  let items = document.querySelectorAll('td');
+
+  let rowLength = row.length;
+  let itemsLength = items.length;
+
+  let itemsInRow = itemsLength /  rowLength;
+
+  for(const key in items) {
+
+    if(items[count] !== undefined) {
+      items[count].style.backgroundColor = 'red';
+    }
+
+    count += (itemsInRow + 1);
+  }
+  
 }
