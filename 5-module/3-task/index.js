5a3d1d6ccec8972eb00f236ca3fda8c899f14243
Dmitriy Sloboda <dmitriy.sloboda@gmail.com>
2020-10-07 20:27:30 +0300

    my solution of this task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
 function initCarousel() {
-  // ваш код...
+  const btns = document.querySelectorAll('.carousel__arrow');
+  const prevBtn = document.querySelector('.carousel__arrow_left');
+  const nextBtn = document.querySelector('.carousel__arrow_right');
+  const slides = document.querySelectorAll('.carousel__slide');
+  const slideWidth = document.querySelector('.carousel__slide').clientWidth;
+  const sliderWidth = slideWidth*slides.length;
+  let cssWidth = 0;
+
+  prevBtn.style.display = 'none';
+
+  for (const item of btns ) {
+
+    item.addEventListener('click', function () {
+
+      if ( item.classList.contains('carousel__arrow_right') ) {
+
+        cssWidth += slideWidth;
+
+        if( cssWidth == sliderWidth - slideWidth ) {
+          nextBtn.style.display = 'none';
+        }
+
+        for ( const item of slides ) {
+          prevBtn.style.display = '';
+          item.style.transform = 'translateX(-'+ cssWidth +'px)';
+        }
+
+      } else {
+        cssWidth -= slideWidth;
+
+        if( cssWidth == 0 ) {
+          prevBtn.style.display = 'none';
+        }
+
+        for ( const item of slides ) {
+          nextBtn.style.display = '';
+          item.style.transform = 'translateX(-'+ cssWidth +'px)';
+        }
+      }
+    });
+  }
 }
